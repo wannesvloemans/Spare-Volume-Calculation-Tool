5f547dce5e848054efd8ad6f93afaa10b77f2747
wannesvloemans <wannesvloemans@gmail.com>
2023-05-03 19:34:48 +0200

    Version two working

--- pdmtools/sparevolumecalculator/views.py
+++ pdmtools/sparevolumecalculator/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from . import helpers
+from . import helpers2
 
 
 # Create your views here.
@@ -27,15 +28,20 @@ def results(request):
         cables = [d/2 for d in cables]
         cables = sorted(cables, reverse=True)
 
-
+        '''
+        CODE FOR VERSION 1 (uncomment if necessary)
+        ----------------------------------------------------------------------------------
         for cable in cables:
-            helpers.add_cable(height, width, cable, placed_cables)
-
+        helpers.add_cable(height, width, cable, placed_cables)
 
         helpers.plot_circles_on_grid(placed_cables, height, width)
         spare_volume = helpers.calculate_spare_volume2(height, width, placed_cables)
+        ----------------------------------------------------------------------------------
+        '''
+
+        helpers2.main_algorithm(cables, height, width)
         
-        return render(request, "sparevolumecalculator/results.html", {'image_file':'/static/images/result.png', 'spare_volume':spare_volume})
+        return render(request, "sparevolumecalculator/results.html", {'image_file':'/static/images/result.png'})#, 'spare_volume':spare_volume})
 
 def documentation(request):
     return render(request, "sparevolumecalculator/documentation.html", {
